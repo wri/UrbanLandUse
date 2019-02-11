fda9d713e173375aa3be755b2db72d115291d777
Peter Kerins <peter.kerins@wri.org>
2019-02-11 17:24:56 -0500

    complete but untested X generator (no Y)

--- utils/tile_generator.py
+++ utils/tile_generator.py
@@ -17,25 +17,40 @@ class TileGenerator(keras.utils.Sequence):
     
     # constructor stuff
     def __init__(self,
-                input_stack,
+                image,
                 pad=32,
 
                 batch_size=128,
                 look_window=17,
+                prep_image=True,
                 ):
-        self.pad = pad
+        if prep_image:
+            self.image = self._prep_image(image)
+        else:
+            self.image = image
+        self.pad=pad
         self.look_window=look_window
-        
-        self._set_data(input_stack,pad)
-        
+        self._set_data(image,pad)
+    
+    # eventually this should all be happening beforehand
+    # want to just pass the prepared, fused input_stack to generator constructor
+    def _prep_image(self,
+                    image):
+        # drop alpha
+        image = image[:-1,:,:]
+        # manual "rescaling" as in all previous phases
+        image = image.astype('float32')
+        image = image/10000.
+        image = np.clip(image,0.0,1.0)
+        return image
+
     def _set_data(self,
-                  input_stack,
+                  image,
                   pad):
-        assert isinstance(input_stack,np.array)
+        assert isinstance(image,np.array)
         # can relax conditions later
-        assert len(input_stack.shape==3)
-        assert input_stack.shape[1]=input_stack.shape[2]
-        self.image = input_stack[:,pad:-pad,pad:-pad]
+        assert len(image.shape==3)
+        assert image.shape[1]=image.shape[2]
         self.size = image.shape[1]^2
         self.batch_size=image.shape[1]
         # for starters, will make columns into batches/steps
@@ -49,8 +64,6 @@ class TileGenerator(keras.utils.Sequence):
 
     def reset(self):
         self.batch_index=-1
-        # randomization step (?)
-        self.dataframe=self.dataframe.sample(frac=1)
 
     def __getitem__(self, index):
         'Generate one batch of data'
@@ -59,63 +72,15 @@ class TileGenerator(keras.utils.Sequence):
         self.batch_index = index
         inputs=self._get_inputs()
         #print 'inputs', inputs.shape
-        return inputs, targets
+        return inputs
 
     def _get_inputs(self):
-        imgs=[]
-        for path in self.rows.path:
-            im = self._read_image(path)
-            im = self._construct_sample(im, self.look_window/2)
-            imgs.append(im)
-        return np.array(imgs)
-    
-    def _read_image(self,path,dtype='uint16'):
-        # read using gdal directly
-        # skip loading any nonessential elements
-        # can add this back in later if we use metadata
-        obj = gdal.Open(path, gdal.gdalconst.GA_ReadOnly)
-        #prj = obj.GetProjection()
-        #geotrans = obj.GetGeoTransform()
-        #cols = obj.RasterXSize
-        #rows = obj.RasterYSize
-        #im = np.zeros((rows,cols), dtype=dtype)
-        im = obj.ReadAsArray().astype(dtype)
-        return im
-    
-    # simple example of more customized input generator
-    def _construct_sample(self, image, look_radius):
-        assert image.shape[1] == image.shape[2]
+        look_radius = self.look_window/2
+        samples=[]
+        for j in range(self.pad,self.image.shape[1]-pad):
+            for i in range(self.pad,self.image.shape[1]-pad):
+                sample = util_rasters.window(self.image,j,i,look_radius,bands_first=True)
+                samples.append(sample)
+        return np.array(samples)
 
-        # manual "rescaling" as in all previous phases
-        image = image.astype('float32')
-        image = image/10000.
-        image = np.clip(image,0.0,1.0)
-
-        # drop alpha
-        image = image[:-1,:,:]
-
-        # grab look window
-        image_side = image.shape[1]
-        center = image_side/2
-        return util_rasters.window(image,center,center,look_radius,bands_first=True)
-
-    def _get_targets(self):
-        categories = list(self.rows.lulc)
-        targets = np.array(categories)
-        if self.remapping is not None:
-            for k in sorted(self.remapping.iterkeys()):
-                targets[targets==k] = self.remapping[k]
-        if self.one_hot:
-            targets = to_categorical(targets)
-        return targets
-
-        
-    
-    # other functions
-
-    def get_label_series(self):
-        mapped_series = self.dataframe['lulc'].map(self.remapping)
-        if mapped_series.isnull().sum() > 0:
-            raise KeyError('remapping does not cover all relevant values, resulting in nan entries')
-        return mapped_series
     
