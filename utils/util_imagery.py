4dac041b8702c96bb5d0b7ba384ed6810ba83c31
Peter Kerins <peter.kerins@wri.org>
2019-02-12 11:05:51 -0500

    moved s2 cloud masking to util_imagery and consolidated via bands_first/last parameter

--- utils/util_imagery.py
+++ utils/util_imagery.py
@@ -47,3 +47,147 @@ def spectral_index(img,a,b,tol=1e-6,bands_first=False):
     a_minus_b = None
     a_plus_b = None
     return y
+
+def ls_haze_removal(img,nodata,thresh=2):
+    #
+    # based on https://github.com/descarteslabs/hedj/blob/master/crops/ml_exp/lacid.py
+    #
+    refl_min = np.array([ 0.04,  0.03,  0.03,  0.03,  0.03,  0.03])  # landsat [B,G,R,NIR,SWIR1,SWIR2]
+    # refl_min = np.array([ 0.0001,  0.0001,  0.0001,  0.0001,  0.0001,  0.0001])  # landsat [B,G,R,NIR,SWIR1,SWIR2]
+    #
+    n_bands = img.shape[0]
+    n_pixels = img.shape[1]*img.shape[2]
+    valid_fraction = (nodata==0).sum()/float(n_pixels)
+    refl_offsets = np.zeros(n_bands,dtype=np.float32)
+    corrected_img = np.zeros(img.shape,dtype=np.float32)
+    #
+    # calculate number of dark pixels
+    nir = img[3]
+    nir_min = refl_min[3]
+    red = img[2]
+    red_min = refl_min[2]
+    # mask = (nir>nir_min) & ((nir<0.1) & (nodata==0))
+    mask = (nir<0.1) & (red>red_min)
+    n_dark_pix = np.sum(mask)
+    #
+    # check that we have enough pixels to work with
+    if ((valid_fraction<0.01) | (n_dark_pix<100)):
+        print "not enough dark pixels for haze correction"
+        return img, refl_offsets
+    #
+    if n_dark_pix>1e5:
+        ds = float(int(n_dark_pix/1e5))
+    else:
+        ds = 1.
+    n_dark_pix /= ds
+    print 'n_dark_pixels: %i' % n_dark_pix
+    #
+    # iterate over bands, measure haze offsets
+    offsets = np.zeros(n_bands,dtype=np.float32)
+    for b in range(len(refl_min)):
+        #
+        # no correction for SWIR
+        if b>3:
+            continue
+        #
+        # this_offset = np.percentile(img[b][mask][::ds,None], thresh)
+        this_offset = np.percentile(img[b][mask], thresh)
+        #
+        # reflectance offsets should be monotonically decreasing
+        if b>0:
+            this_offset = np.min([this_offset, offsets[b-1]])
+        offsets[b] = this_offset
+    #
+    # APPLY CORRECTION
+    refl_offsets = offsets - refl_min
+    refl_offsets[[4,5]] = 0.0    
+    for b in range(n_bands):
+        corrected_img[b][:,:] = img[b][:,:] - refl_offsets[b]
+        corrected_img[b] = np.clip(corrected_img[b],0.0,1.5)
+        print b, offsets[b], refl_offsets[b], img[b].min(), corrected_img[b].min(), corrected_img[b].max() 
+    #
+    return corrected_img, refl_offsets
+
+def s2_cloud_mask(X,get_rgb=False, 
+                  key={0:'nodata',1:'land',2:'snow',3:'water',4:'clouds',5:'vegetation',6:'shadows'},
+                  bands_first=False):
+    #
+    #    clouds are bright, visibly gray, and relatively cool
+    #
+    #    inputs:
+    #    X       6-band landsat images : VIS/NIR/SWIR bands[1,2,3,4,5,7] in top-of-atmosphere reflectance
+    #
+    #    output:
+    #    Y       byte-valued cloud/snow/water/shadow mask
+    # 
+    #    vals:   (based on official NASA LTK cloud mask labels)
+    #    1       land
+    #    2       snow
+    #    3       water bodies
+    #    4       clouds
+    #    5       vegetation
+    #    6       shadows  (extra category not in the standard NASA label set)
+    #
+    if bands_first:
+        L1 = X[0,:,:]
+        L2 = X[1,:,:]
+        L3 = X[2,:,:]
+        L5 = X[4,:,:]
+        alpha = X[-1,:,:]
+    else:
+        L1 = X[:,:,0]
+        L2 = X[:,:,1]
+        L3 = X[:,:,2]
+        L5 = X[:,:,4]
+        alpha = X[:,:,-1]
+    #
+    # land - default value
+    #
+    Y = np.ones(L1.shape,dtype='uint8')
+    #
+    # snow/ice
+    #
+
+    ndsi = spectral_index(X,1,4,bands_first=bands_first)
+    Y[(ndsi > 0.4)]  = 2  
+    #
+    ndvi = spectral_index(X,3,2,bands_first=bands_first)
+    #
+    # Y[(ndvi <= 0.0)] = 3  
+    #
+    # shadows
+    #
+    shadow_index = (ndvi <= 0.0)
+    Y[shadow_index] = 6
+    #
+    # water
+    #
+    ratioBR = np.divide(L1,np.add(L3,1e-6))
+    water_index = np.logical_and((ndvi <= 0.0),(ratioBR >= 1.0))
+    Y[water_index] = 3  # water
+    #
+    # vegetation
+    #
+    Y[(ndvi > 0.2)] = 5
+    #
+    # CLOUD INDEX
+    # 
+    index = (L2 >= 0.20)  # 0.40
+    #
+    grey = spectral_index(X,1,0,bands_first=bands_first)
+    index = np.logical_and(index, (abs(grey) < 0.2))    
+    grey = spectral_index(X,2,1,bands_first=bands_first)
+    index = np.logical_and(index, (abs(grey) < 0.2))
+    Y[index] = 4  # clouds
+    #
+    # nodata 
+    #
+    Y[(alpha==0)]=0
+    #
+    if (get_rgb==True):
+        rgb = rgb_clouds(Y)
+        return Y,rgb,key
+    #
+    return Y,key
+
+
